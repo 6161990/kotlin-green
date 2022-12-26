006f27dfccddff30e5c2ea7603b12323b279cd9e
6161990 <6161990src@gmail.com>
2022-12-26 16:16:26 +0900

    component N , 구조 분해 할당

--- src/main/kotlin/com/yoon/lang/advanced/Advanced02.kt
+++ src/main/kotlin/com/yoon/lang/advanced/Advanced02.kt
@@ -1,6 +1,7 @@
 package com.yoon.lang.advanced
 data class Person(val name:String, val age: Int)
 class NotDataPerson(val name:String, val age: Int)
+data  class MutablePerson(var name:String, val age: Int)
 
 fun main() {
     /** equals : 두 개의 인스턴스 데이터에 대한 동등성 비교 */
@@ -22,5 +23,31 @@ fun main() {
     /** toString : 레퍼런스 주소 값이 아닌 객체의 프로퍼티 값을 알 수 있도록 나온다. */
     println(person1)
 
+
+    /** copy : 객체의 불변성을 쉽게 유지할 수 있다. */
+    val mutablePerson = MutablePerson("dodo", 29)
+    mutablePerson.name = "ro"
+    println(mutablePerson)
+
+    // 불변성이 깨졌을 때의 문제점
+    // 동기화 , 멀티 스레드 에서의 안정성
+    val mutablePerson2 = MutablePerson("nana", 29)
+    val hashSet = hashSetOf(mutablePerson2)
+    println(hashSet.contains(mutablePerson2)) // true
+
+    mutablePerson2.name = "kaka"
+    println(hashSet.contains(mutablePerson2)) // false
+
+    // 객체를 수정 하는 것보다 객체를 복사하여 변경해 사용하는 것이 안정
+    val copied = mutablePerson2.copy(name = "lin")
+    println(copied)
+
+    /** component N */
+    val person = Person("soo", 20)
+    println("이름 = ${person.component1()}, 나이 = ${person.component2()}")
+
+    /** 구조분해 할당 */
+    val (name, age) = person
+    println("이름 = ${name}, 나이 = ${age}")
 
 }
