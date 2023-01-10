04a0679d0c5e1087e68675bd25b7954aa63649db
6161990 <6161990src@gmail.com>
2023-01-10 12:17:27 +0900

    @JvmStatic  이용하여 정적 함수 호출하기

---
+++ src/main/kotlin/com/yoon/lang/kotilnInJava/useJvmStack/HelloClass.kt
@@ -0,0 +1,28 @@
+package com.yoon.lang.kotilnInJava.useJvmStack
+
+class HelloClass {
+
+    /** companion object 함께 정의된 싱글톤 객체의 함수를 코틀린에서 사용하면 자바의 static 메소드와 비슷하다 */
+    companion object {
+
+        @JvmStatic
+        fun hello() = "hello"
+
+    }
+}
+
+object HiObject {
+
+    @JvmStatic
+    fun hi() = "hi"
+}
+
+fun main() {
+    /** static 하게 사용가능 */
+    val helloClass = HelloClass.hello()
+    val hi = HiObject.hi()
+
+    println(helloClass)
+    println(hi)
+
+}
---
+++ src/main/kotlin/com/yoon/lang/kotilnInJava/useJvmStack/JvmStaticExample.java
@@ -0,0 +1,21 @@
+package com.yoon.lang.kotilnInJava.useJvmStack;
+
+public class JvmStaticExample {
+
+    public static void main(String[] args) {
+        String hello = HelloClass.Companion.hello(); // Companion 키워드 뺄 수 없음
+        String hi = HiObject.INSTANCE.hi(); // INSTANCE 키워드 뺄 수 없음
+
+        System.out.println(hello);
+        System.out.println(hi);
+
+
+        /** @JvmStatic 키워드를 이용하여 바로 접근할 수 있음 */
+        String hello2 = HelloClass.hello();
+        String hi2 = HiObject.hi();
+
+        System.out.println(hello2);
+        System.out.println(hi2);
+
+    }
+}
