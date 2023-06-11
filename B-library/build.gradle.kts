4d3292a41a8c829fee8c643c232ac098fae6a40b
6161990 <6161990src@gmail.com>
2023-06-11 16:57:06 +0900

    init b library

---
+++ B-library/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("java")
+    kotlin("plugin.jpa") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.yoon"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.projectlombok:lombok:1.18.24")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@
 rootProject.name = "kotlin-green"
 include("kotlin-red")
 include("kotlin-todo")
+include("B-library")
