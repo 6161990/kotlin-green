718d4ce080962b23ae1e27902268415c152ec020
6161990 <6161990src@gmail.com>
2023-06-13 08:07:06 +0900

    init kotlin demo

---
+++ library-app/src/main/java/com/yoon/libraryapp/dto/user/request/UserUpdateRequest.java
@@ -0,0 +1,16 @@
+package com.yoon.libraryapp.dto.user.request;
+
+public class UserUpdateRequest {
+
+  private long id;
+  private String name;
+
+  public long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+}
---
+++ library-app/src/main/java/com/yoon/libraryapp/service/book/BookService.java
@@ -0,0 +1,54 @@
+package com.yoon.libraryapp.service.book;
+
+import com.yoon.libraryapp.domain.book.Book;
+import com.yoon.libraryapp.domain.book.BookRepository;
+import com.yoon.libraryapp.domain.user.User;
+import com.yoon.libraryapp.domain.user.UserRepository;
+import com.yoon.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
+import com.yoon.libraryapp.dto.book.request.BookLoanRequest;
+import com.yoon.libraryapp.dto.book.request.BookRequest;
+import com.yoon.libraryapp.dto.book.request.BookReturnRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookService {
+
+  private final BookRepository bookRepository;
+  private final UserRepository userRepository;
+  private final UserLoanHistoryRepository userLoanHistoryRepository;
+
+  public BookService(
+      BookRepository bookRepository,
+      UserRepository userRepository,
+      UserLoanHistoryRepository userLoanHistoryRepository
+  ) {
+    this.bookRepository = bookRepository;
+    this.userRepository = userRepository;
+    this.userLoanHistoryRepository = userLoanHistoryRepository;
+  }
+
+  @Transactional
+  public void saveBook(BookRequest request) {
+    Book newBook = new Book(request.getName());
+    bookRepository.save(newBook);
+  }
+
+  @Transactional
+  public void loanBook(BookLoanRequest request) {
+    Book book = bookRepository.findByName(request.getBookName()).orElseThrow(IllegalArgumentException::new);
+    if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.getBookName(), false) != null) {
+      throw new IllegalArgumentException("진작 대출되어 있는 책입니다");
+    }
+
+    User user = userRepository.findByName(request.getUserName()).orElseThrow(IllegalArgumentException::new);
+    user.loanBook(book);
+  }
+
+  @Transactional
+  public void returnBook(BookReturnRequest request) {
+    User user = userRepository.findByName(request.getUserName()).orElseThrow(IllegalArgumentException::new);
+    user.returnBook(request.getBookName());
+  }
+
+}
