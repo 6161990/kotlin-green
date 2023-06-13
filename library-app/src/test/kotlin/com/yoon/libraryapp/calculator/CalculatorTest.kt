f2741e176d3a16892aa07a5fb8aa36cf911189e0
6161990 <6161990src@gmail.com>
2023-06-14 00:07:46 +0900

    addTest

--- library-app/src/test/kotlin/com/yoon/libraryapp/calculator/CalculatorTest.kt
+++ library-app/src/test/kotlin/com/yoon/libraryapp/calculator/CalculatorTest.kt
@@ -6,6 +6,9 @@ import java.lang.IllegalStateException
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
 }
 
 class CalculatorTest {
@@ -14,6 +17,33 @@ class CalculatorTest {
         calculator.add(1)
 
         if(calculator.number != 6){
+            throw IllegalStateException("expected != calculator")
+        }
+    }
+
+    fun minusTest() {
+        val calculator = Calculator(5)
+        calculator.minus(1)
+
+        if(calculator.number != 4){
+            throw IllegalStateException("expected != calculator")
+        }
+    }
+
+    fun multiplyTest() {
+        val calculator = Calculator(5)
+        calculator.multiply(1)
+
+        if(calculator.number != 5){
+            throw IllegalStateException("expected != calculator")
+        }
+    }
+
+    fun divideTest() {
+        val calculator = Calculator(5)
+        calculator.divide(5)
+
+        if(calculator.number != 1){
             throw IllegalStateException("expected != calculator")
         }
     }
