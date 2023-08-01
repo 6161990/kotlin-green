37a123353be051a034c2c818bf2330c5f45421a8
6161990 <6161990src@gmail.com>
2023-08-01 22:18:53 +0900

    modified dependencies

--- kotest/settings.gradle.kts
+++ kotest/settings.gradle.kts
@@ -1,2 +1,51 @@
 rootProject.name = "kotest"
 include("testing")
+
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.plugin.allopen" -> {
+                    val kotlinVersion: String by settings
+                    useVersion(kotlinVersion)
+                }
+
+                "org.jetbrains.kotlin.jvm" -> {
+                    val kotlinVersion: String by settings
+                    useVersion(kotlinVersion)
+                }
+
+                "org.jetbrains.kotlin.kapt" -> {
+                    val kotlinVersion: String by settings
+                    useVersion(kotlinVersion)
+                }
+
+                "org.jetbrains.kotlin.plugin.jpa" -> {
+                    val kotlinVersion: String by settings
+                    useVersion(kotlinVersion)
+                }
+
+                "org.jetbrains.kotlin.plugin.spring" -> {
+                    val kotlinVersion: String by settings
+                    useVersion(kotlinVersion)
+                }
+
+                "org.springframework.boot" -> {
+                    val springBootVersion: String by settings
+                    useVersion(springBootVersion)
+                }
+
+                "io.spring.dependency-management" -> {
+                    val springDependencyManagementVersion: String by settings
+                    useVersion(springDependencyManagementVersion)
+                }
+
+                "org.jlleitschuh.gradle.ktlint" -> {
+                    val ktlintVersion: String by settings
+                    useVersion(ktlintVersion)
+                }
+
+            }
+        }
+    }
+}
--- kotest/testing/build.gradle.kts
+++ kotest/testing/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    id("java")
-    kotlin("jvm") version "1.9.0"
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("plugin.spring")
+    kotlin("jvm")
 }
 
 group = "com.yoon"
@@ -10,9 +12,27 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion: String by rootProject
+val mockkVersion: String by rootProject
+val kotestExtensionsSpringVersion: String by rootProject
+val mockitoVersion: String by rootProject
+val kotlinCoroutinesVersion: String by rootProject
+
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    apply(plugin = "kotlin")
+    apply(plugin = "kotlin-kapt")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-devtools")
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutinesVersion")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpringVersion")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockitoVersion")
+
 }
 
 tasks.getByName<Test>("test") {
